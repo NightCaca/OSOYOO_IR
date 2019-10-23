a48ab64d109c2cb9418cfb5130fadebd805439eb
NightCaca <42962355+NightCaca@users.noreply.github.com>
2019-10-23 11:42:56 +0800

    Create ir.ts

---
+++ ir.ts
@@ -0,0 +1,20 @@
+    //% color="#31C7D5" weight=19
+    //% icon="\uf1eb"
+declare namespace OSOYOO_IR {
+
+    /**
+     * button pushed.
+     */
+    //% blockId=ir_received_left_event
+    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
+
+    /**
+     * initialises local variablesssss
+     */
+    //% blockId=ir_init
+    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    export function init(pin: Pins): void;
+}
+
+// Auto-generated. Do not edit. Really.
